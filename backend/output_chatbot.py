a5d6423f7fe8b4cc1e9a473ed1312f129babbbbf
Abhyoday Singh <abhyodays@gmail.com>
2023-04-29 00:34:16 +0530

    Added chatbot

--- backend/output_chatbot.py
+++ backend/output_chatbot.py
@@ -7,7 +7,7 @@ from sentiment_analysis import get_emotion
 app = Flask(__name__)
 
 intents = json.loads(open("intents.json",encoding="utf8").read())
-
+chat_history = []
 
 @app.route('/')
 def main():
@@ -20,13 +20,13 @@ def chatbot():
     ints = predict_class(message)
     res = get_response(ints, intents)
     emotion = get_emotion(message)
-
-    # return render_template("index.html", result=res, emotion=emotion)
     temp = {"res":res, "emotion":emotion}
+    chat_history.append({"req":message,"res" : res})
     return json.dumps(temp)
-@app.route('/chatbot')
+
+@app.route('/chatbot', methods=['GET','POST'])
 def loadChatbot():
-    return render_template("chatbot_template.html")
+    return render_template("chatbot_template.html", chat_history=chat_history)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
