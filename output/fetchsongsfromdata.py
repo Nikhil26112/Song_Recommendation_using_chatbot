fbaca4958a8073bdfb001c419c9e111fb49fb081
Nikhil <nikhilchauhan2611@gmail.com>
2023-04-29 01:40:47 +0530

    added frontend files

--- output/fetchsongsfromdata.py
+++ output/fetchsongsfromdata.py
@@ -4,7 +4,7 @@ import csv
 import json
 
 # Open the CSV file and create a DictReader object
-with open('Spotify-Machine-Learning/data/data.csv', 'r') as csvfile:
+with open('../Spotify-Machine-Learning/data/data.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     json_list = []
     for row in reader:
@@ -25,8 +25,22 @@ for obj in json_list:
     mood = obj['mood']
     classified_data[mood].append(obj)
 
+
+suggestion_list = []
+if emotion == "joy":
+    suggestion_list = random.sample(classified_data['Energetic'], 10)
+elif emotion == "happy":
+    suggestion_list = random.sample(classified_data['Happy'], 10)
+elif emotion == "sad":
+    suggestion_list = random.sample(classified_data['Sad'], 10)
+elif emotion == "disgust":
+    suggestion_list = random.sample(classified_data['Calm'], 10)
+elif emotion == "fear":
+    suggestion_list = random.sample(classified_data['Calm'], 10)
+
+
 # Write the classified data to separate JSON files
-for mood, objects in classified_data.items():
-    filename = f'{mood}.json'
-    with open(filename, 'w') as f:
-        json.dump(objects, f, indent=4)
+# for mood, objects in classified_data.items():
+#     filename = f'{mood}.json'
+#     with open(filename, 'w') as f:
+#         json.dump(objects, f, indent=4)
