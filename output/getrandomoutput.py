2939d8e666122be8a222941f59e3ea7dd0fbc161
Nikhil <nikhilchauhan2611@gmail.com>
2023-04-29 00:39:34 +0530

    .

---
+++ output/getrandomoutput.py
@@ -0,0 +1,13 @@
+import random
+import pandas as pd
+
+if emotion == "joy":
+    pass
+elif emotion == "happy":
+    pass
+elif emotion == "sad":
+    pass
+elif emotion == "disgust":
+    pass
+elif emotion == "fear":
+    pass
