bd56ad6568c51f4a2efbb404eb189ba56e6715d5
Abhyoday Singh <abhyodays@gmail.com>
2023-05-29 02:31:33 +0530

    Added latest release

--- backend/output_chatbot.py
+++ backend/output_chatbot.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from chatbot import predict_class, get_response
 import json, random, csv
+from  get_playlist import get_latest_releases
 # import request
 from sentiment_analysis import get_emotion
 
@@ -11,6 +12,7 @@ intents = json.loads(open("intents.json", encoding="utf8").read())
 
 chat_history = []
 suggestion_list = []
+latest_releases = get_latest_releases()
 
 def random_songs(emotion):
         global suggestion_list
@@ -58,9 +60,10 @@ def chatbot():
         temp = {"res": res, "emotion": emotion}
         print("req:", message)
         print(temp)
+        get_latest_releases()
         chat_history.append({"req": message, "res": res})
         return redirect(url_for('chatbot'))
-    return render_template('chatbot.html', chat_history=chat_history, suggestion_list=suggestion_list)
+    return render_template('chatbot.html', chat_history=chat_history, suggestion_list=suggestion_list,latest_releases = latest_releases)
 #     # return json.dumps(temp)
 
 
